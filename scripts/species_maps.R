b654d4afaa71ee26ffe1eedb8786dafa9ef18ccd
rbolam <friederike.bolam@ncl.ac.uk>
2020-02-11 11:23:17 +0000

    Change maps to PDF

--- scripts/species_maps.R
+++ scripts/species_maps.R
@@ -7,38 +7,96 @@ library(rworldmap)
 library(ggalt)
 library(gridExtra)
 
-cand <- read.csv("data/candidate_countries.csv")
+cand <- read.csv("data/allspp_medians.csv")
+
+countries <- read.csv("data/RL_data_29_01_2020/countries.csv")
+
+rl_cat <- read.csv("data/RL_data_29_01_2020/simple_summary.csv")
 
 
 #### --------------------------------- Sort data ----------------------------------####
 
-## Count number in each class by country for candidates, for 1993, and for 2010, & make list
+
+## Select best estimate for all candidates only
+
+cand <- cand %>% 
+  filter(est == "Best") %>% 
+  select(Scientific.name, class, median1993, median2010)
+
+
+## Sort country data, retain native and reintroduced ranges only, merge with candidates
+
+countries %>% 
+  select(scientificName, name, presence, origin) %>% 
+  rename(Scientific.name = scientificName) %>% 
+  right_join(cand, by = "Scientific.name") %>% 
+  rename(region = name) %>% 
+  filter(origin %in% c("Native", "Reintroduced")) %>% 
+  mutate_if(is.factor, as.character) ->
+  cand
+
+
+## Mergw with current RL categories
+
+rl_cat <- rl_cat %>% 
+  select(scientificName, redlistCategory) %>% 
+  rename(Scientific.name = scientificName)
+
+cand <- left_join(cand, rl_cat, by = "Scientific.name")
+
+
+## Remove range where extant spp are extinct, but retain extinct ranges for EW spp
+
+ext <- filter(cand, presence %in% c("Extant", "Possibly Extant") & redlistCategory != "Extinct in the Wild")
+exw <- filter(cand, presence == "Extinct Post-1500" & redlistCategory == "Extinct in the Wild")
+
+cand <- bind_rows(ext, exw)
+
+
+## Make sure country names are correctly mappable
+
+cand$region[cand$region == "Korea, Democratic People's Republic of"] <- c("North Korea")
+cand$region[cand$region == "Korea, Republic of"] <- c("South Korea")
+cand$region[cand$region %in% c("Russian Federation (Eastern Asian Russia)", "Russian Federation")] <- c("Russia")
+cand$region[cand$region == "Syrian Arab Republic"] <- c("Syria")
+cand$region[cand$region == "Taiwan, Province of China"] <- c("Taiwan")
+cand$region[cand$region %in% c("United States", "United States Minor Outlying Islands")] <- c("USA")
+cand$region[cand$region == "Viet Nam"] <- c("Vietnam")
+cand$region[cand$region == "Hong Kong"] <- c("China")
+cand$region[cand$region == "RÃ©union"] <- c("Reunion")
+cand$region[cand$region == "Brunei Darussalam"] <- c("Brunei")
+cand$region[cand$region == "Cabo Verde"] <- c("Cape Verde")
+cand$region[cand$region == "French Southern Territories"] <- c("French Southern and Antarctic Lands")
+cand$region[cand$region == "Lao People's Democratic Republic"] <- c("Laos")
+
+
+#### ------------------------------- Count numbers --------------------------------####
 
 
 ## For candidate species
 
 aggcand <- cand %>% 
-  group_by(region, className) %>% 
+  group_by(region, class) %>% 
   count() %>% 
-  spread(className, n, fill = NA)
+  spread(class, n, fill = NA)
 
 
 ## For 1993 species
 
 agg1993 <- cand %>% 
-  filter(!is.na(t1993)) %>% 
-  group_by(region, className) %>% 
+  filter(median1993 > 50) %>% 
+  group_by(region, class) %>% 
   count() %>% 
-  spread(className, n, fill = NA)
+  spread(class, n, fill = NA)
 
 
 ## For 2010 species
 
 agg2010 <- cand %>% 
-  filter(!is.na(t2010)) %>% 
-  group_by(region, className) %>% 
+  filter(median2010 > 50) %>% 
+  group_by(region, class) %>% 
   count() %>% 
-  spread(className, n, fill = NA)
+  spread(class, n, fill = NA)
 
 
 ## Make list of 3 dataframes
@@ -79,7 +137,7 @@ for (i in 1:3) {
   ## Make bird plot
   ggplot() + 
     geom_map(data = map.all, map = map.all, 
-             aes(map_id = region, x = long, y = lat, fill = AVES), colour = "black", size = 0.01) + 
+             aes(map_id = region, x = long, y = lat, fill = AVES), colour = "black", size = 0.1) + 
     coord_proj("+proj=cea +lat_ts=37.5") +
     labs(tag = "a) Birds       ", x = "", y = "") + 
     # add some circles around island nations:
@@ -93,7 +151,7 @@ for (i in 1:3) {
   ## Make mammal plot
   ggplot() + 
     geom_map(data = map.all, map = map.all,
-             aes(map_id = region, x = long, y = lat, fill = MAMMALIA), colour = "black", size = 0.01) + 
+             aes(map_id = region, x = long, y = lat, fill = MAMMALIA), colour = "black", size = 0.1) + 
     coord_proj("+proj=cea +lat_ts=37.5") +
     labs(tag = "b) Mammals", x = "", y = "") +
     guides(colour = "none") +
@@ -119,9 +177,9 @@ plotbcand <- plotb[[1]] +
   scale_fill_gradient2(low = "#edf8b1", mid = "#41b6c4", high = "#0c2c84", midpoint = 5.5, guide = "legend") + 
   theme(legend.position = "none")
 
-tiff("output/candidatemap.tiff", width = 7.08661, height = 7, res = 300, units = "in")
-plot <- grid.arrange(plotacand, plotbcand, heights = c(1.07, 1))
-dev.off()
+mapcand <- grid.arrange(plotacand, plotbcand, heights = c(1.07, 1))
+ggsave("output/candidatemap.pdf", mapcand, width = 7.08661, height = 7, unit = "in", dpi = 300)
+
 
 
 ## Make 1993 plot with adjusted colour scales and save
@@ -138,9 +196,9 @@ plotb1993 <- plotb[[2]] +
   scale_fill_gradient2(low = "#edf8b1", mid = "#41b6c4", high = "#0c2c84", midpoint = 3.5, guide = "legend") +
   theme(legend.position = "none")
 
-tiff("output/1993map.tiff", width = 7.08661, height = 7, res = 600, units = "in")
-plot <- grid.arrange(plota1993, plotb1993, heights = c(1.07, 1))
-dev.off()
+map1993 <- grid.arrange(plota1993, plotb1993, heights = c(1.07, 1))
+ggsave("output/1993map.pdf", map1993, width = 7.08661, height = 7, unit = "in", dpi = 1000)
+
 
 
 ## Make 2010 plot with adjusted colour scales and save
@@ -156,8 +214,7 @@ plotb2010 <- plotb[[3]] +
   scale_fill_gradient2(low = "#edf8b1", mid = "#41b6c4", high = "#0c2c84", midpoint = 3, guide = "legend") +
   theme(legend.position = "none")
 
-tiff("output/2010map.tiff", width = 7.08661, height = 7, res = 300, units = "in")
-plot <- grid.arrange(plota2010, plotb2010, heights = c(1.07, 1))
-dev.off()
+map2010 <- grid.arrange(plota2010, plotb2010, heights = c(1.07, 1))
+ggsave("output/2010map.pdf", map2010, width = 7.08661, height = 7, unit = "in", dpi = 300)
 
 
