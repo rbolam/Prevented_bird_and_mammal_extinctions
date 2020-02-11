6f4b09008e3603a14c12c721f3ef283661458ef4
rbolam <friederike.bolam@ncl.ac.uk>
2020-02-11 11:32:06 +0000

    Add data wrangling

--- scripts/threats_plot.R
+++ scripts/threats_plot.R
@@ -5,8 +5,8 @@
 library(tidyverse)
 library(gridExtra)
 
-threats <- read.csv("data/threats.csv", na.strings = c("NA", ""))
-allspp <- read.csv("data/all_info_candidates.csv")
+threats <- read.csv("data/RL_data_29_01_2020/threats.csv", na.strings = c("NA", ""))
+allspp <- read.csv("data/allspp_medians.csv")
 
 
 #### ----------------------------------Sort data ----------------------------------####
@@ -19,7 +19,9 @@ threats <- threats %>%
   select(scientificName, name, code, ias) %>% 
   rename(Scientific.name = scientificName) 
 
-allspp <- select(allspp, className, Scientific.name, t1993, t2010)
+allspp <- allspp %>% 
+  filter(est == "Best") %>% 
+  select(class, Scientific.name, median1993, median2010)
 
 
 ## Merge threats and candidate list
@@ -41,7 +43,6 @@ tnames <- data.frame(cbind(threat_level1, tnames))
 
 threats <- left_join(threats, tnames, by = "threat_level1")  
 threats$threat_level1 <- as.integer(threats$threat_level1)
-
 
 
 ## Manually change level 1 threats Biological Resource Use and Natural Systems Modifications to level 2 threats 
@@ -67,8 +68,8 @@ threats <- filter(threats, !is.na(code))
 ## Make 3 separate dfs for cand, 1993 and 2010
 
 
-thr1993 <- filter(threats, !is.na(t1993))
-thr2010 <- filter(threats, !is.na(t2010))
+thr1993 <- filter(threats, median1993 > 50)
+thr2010 <- filter(threats, median2010 > 50)
 
 alldfs <- list(threats, thr1993, thr2010)
   
@@ -81,7 +82,7 @@ plotb <- list()
 
 for (i in 1:3) {
   alldfs[[i]] %>% 
-  filter(className == "AVES") %>% 
+  filter(class == "AVES") %>% 
   select(Scientific.name, tnames) %>% 
   unique() %>% 
   count(tnames) %>% 
@@ -94,7 +95,7 @@ for (i in 1:3) {
   plota[[i]]
   
   alldfs[[i]] %>% 
-  filter(className == "MAMMALIA") %>% 
+  filter(class == "MAMMALIA") %>% 
   select(Scientific.name, tnames) %>% 
   unique() %>% 
   count(tnames) %>% 
@@ -130,7 +131,7 @@ c1993 <- grid.arrange(plota1993, plotb1993, ncol = 1)
 ggsave("output/1993threats.pdf", c1993, width = 8, height = 12, unit = "cm", dpi = 600)
 
 
-## 1993 plot
+## 2010 plot
 
 (plota2010 <- plota[[3]] + coord_flip(ylim = c(0.7, 16)))
 (plotb2010 <- plotb[[3]] + coord_flip(ylim = c(0.7, 16)))
